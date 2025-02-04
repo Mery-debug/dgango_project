99732be44c1f018f108533bb93daf19e158d8404
Mery-debug <m.a.shap58000@gmail.com>
2025-02-04 22:56:43 +0300

    fixture

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+# admin.site.register(Category)
+# admin.site.register(Product)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)

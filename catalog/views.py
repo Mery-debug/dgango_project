829694bf3998abe185bdea7104331ae640ed1980
Mery-debug <m.a.shap58000@gmail.com>
2025-02-12 17:51:25 +0300

    contr

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def catalog_view_home(request):
     return render(request, 'catalog/home.html')
@@ -10,6 +12,14 @@ def catalog_view_contacts(request):
     return render(request, 'catalog/contacts.html')
 
 
-def item_view(request):
+def product_list(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'catalog/products.html', context)
 
-    return render(request, 'catalog/item.html')
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    context = {'product': product}
+    return render(request, 'catalog/product.html', context)
+

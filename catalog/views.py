0fc66a6d6b6e9e7ce8dba631167cab98bd77f384
Mery-debug <m.a.shap58000@gmail.com>
2025-02-19 15:23:46 +0300

    views

--- catalog/views.py
+++ catalog/views.py
@@ -1,24 +1,40 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.views.generic.edit import CreateView, UpdateView, View, DeleteView
+from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
 from .models import Product
 
 
-def catalog_view_home(request):
-    return render(request, 'catalog/home.html')
+class CatalogHomeView(View):
+    template_name = 'catalog/home.html'
+    success_url = reverse_lazy('home')
 
 
-def catalog_view_contacts(request):
-    return render(request, 'catalog/contacts.html')
+class CatalogContactsView(View):
+    template_name = 'catalog/contacts.html'
+    success_url = reverse_lazy('contacts')
 
 
-def product_list(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'catalog/products.html', context)
+class CatalogViewList(ListView):
+    model = Product
+    context_object_name = 'products'
+    template_name = 'catalog/products.html'
+    success_url = reverse_lazy('product_list')
 
 
-def product_detail(request, product_id=Product.id):
-    product = get_object_or_404(Product, pk=product_id)
-    context = {'product': product}
-    return render(request, 'catalog/product.html', context)
+class CatalogViewDetail(DetailView):
+    model = get_object_or_404(Product, pk=Product.id)
+    context_object_name = 'product'
+    template_name = 'catalog/product.html'
+    success_url = reverse_lazy('product_details')
 
+
+class CatalogCreateView(CreateView):
+    model = Product
+    fields = ['id', 'name', 'description', 'category', 'price', 'img']
+    template_name = 'catalog/create.html'
+    success_url = reverse_lazy('product_create')
+
+
+class CatalogUpdateView(UpdateView):
+    pass

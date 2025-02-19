73a869f202e532a8dee034db368542f1fc38e06b
Mery-debug <m.a.shap58000@gmail.com>
2025-02-20 02:56:50 +0300

    1,3

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.edit import CreateView, UpdateView, View, DeleteView
 from django.views.generic import ListView, DetailView
-from django.urls import reverse_lazy, reverse
+from django.urls import reverse_lazy
 from .models import Product
 
 
@@ -18,26 +18,42 @@ class CatalogViewList(ListView):
     model = Product
     context_object_name = 'products'
     template_name = 'catalog/products.html'
-    success_url = reverse_lazy('product_list/')
+    success_url = reverse_lazy('product_list')
+
+    def get_queryset(self):
+        return Product.objects.filter(is_active=True)
 
 
 class CatalogViewDetail(DetailView):
     model = Product
     context_object_name = 'product'
     template_name = 'catalog/product.html'
-    success_url = reverse_lazy('product_details/')
+    success_url = reverse_lazy('product_details')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product'].img = getattr(context['product'], 'img', None)
+        return context
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        if obj.is_active:
+            obj.view_count += 1
+            obj.save()
+            return obj
+        else:
+            return None
 
 class CatalogCreateView(CreateView):
     model = Product
-    fields = ['id', 'name', 'descriptions', 'category', 'price']
+    fields = ['id', 'name', 'descriptions', 'category', 'price', 'img']
     template_name = 'catalog/create.html'
-    success_url = reverse_lazy('product_list/')
+    success_url = reverse_lazy('catalog:product_list')
 
 
 class CatalogUpdateView(UpdateView):
     model = Product
-    fields = ['name', 'description', 'category', 'price', 'img']
+    fields = ['name', 'descriptions', 'category', 'price', 'img']
     template_name = 'catalog/update.html'
     success_url = reverse_lazy('product/<int:pk>/edit/')
 
@@ -45,5 +61,5 @@ class CatalogUpdateView(UpdateView):
 class CatalogDeleteView(DeleteView):
     model = Product
     template_name = 'catalog/confirm_delete.html'
-    success_url = reverse_lazy('product_list/')
+    success_url = reverse_lazy('catalog:product_list')
 

117f2683797c7ca56f7f8b99ea9bbadc6943c1eb
Mery-debug <m.a.shap58000@gmail.com>
2025-02-26 18:04:45 +0300

    2

---
+++ catalog/forms.py
@@ -0,0 +1,61 @@
+import os
+
+from django import forms
+from jedi.inference.value import instance
+
+from .models import Product, Category
+from django.core.exceptions import ValidationError
+from dotenv import load_dotenv
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'descriptions', 'category', 'price', 'img']
+
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
+        self.update_field_attributes()
+
+        self.fields['category'].widget.attrs.update({
+            'class': 'form-control',
+        })
+
+    def update_field_attributes(self):
+
+        for field_name in self.fields:
+            self.fields[field_name].widget.attrs.update({
+                'class': 'form-control',
+                'placeholder': f'Введите {}'
+            })
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        load_dotenv()
+        lst_exception = os.getenv('LST_EXCEPTION')
+        if lst_exception in name.lower():
+            raise ValidationError('Вы ввели запрещенное слово')
+        return name
+
+    def clean_descriptions(self):
+        descriptions = self.cleaned_data.get('descriptions')
+        load_dotenv()
+        lst_exception = os.getenv('LST_EXCEPTION')
+        if lst_exception in descriptions.lower():
+            raise ValidationError('Вы ввели запрещенное слово')
+        return descriptions
+
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if price <= 0:
+            raise ValidationError('Цена не может быть ниже нуля или равняться нулю')
+        return price
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['description', 'name']
+
+
+
--- catalog/views.py
+++ catalog/views.py
@@ -1,19 +1,75 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, View, DeleteView
+from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+
+from .forms import ProductForm
+from .models import Product
 
 
-def catalog_view_home(request):
-    return render(request, 'catalog/home.html')
+class CatalogHomeView(View):
+    template_name = 'catalog/home.html'
+    success_url = reverse_lazy('home')
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
 
 
-def catalog_view_contacts(request):
-    return render(request, 'catalog/contacts.html')
+class CatalogContactsView(View):
+    template_name = 'catalog/contacts.html'
+    success_url = reverse_lazy('contacts')
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
 
 
-# def contact(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         phone = request.POST.get('phone')
-#         message = request.POST.get('message')
-#         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
-#     return render(request, 'students/contact.html')
+class CatalogViewList(ListView):
+    model = Product
+    context_object_name = 'products'
+    template_name = 'catalog/products.html'
+    success_url = reverse_lazy('product_list')
+
+    def get_queryset(self):
+        return Product.objects.filter(is_active=True)
+
+
+class CatalogViewDetail(DetailView):
+    model = Product
+    context_object_name = 'product'
+    template_name = 'catalog/product.html'
+    success_url = reverse_lazy('product_details')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product'].img = getattr(context['product'], 'img', None)
+        return context
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        if obj.is_active:
+            obj.view_count += 1
+            obj.save()
+            return obj
+        else:
+            return None
+
+
+class CatalogCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/create.html'
+    success_url = reverse_lazy('catalog/product_list')
+
+
+class CatalogUpdateView(UpdateView):
+    model = Product
+    fields = ['name', 'descriptions', 'category', 'price', 'img']
+    template_name = 'catalog/update.html'
+    success_url = reverse_lazy('product/<int:pk>/edit/')
+
+
+class CatalogDeleteView(DeleteView):
+    model = Product
+    template_name = 'catalog/confirm_delete.html'
+    success_url = reverse_lazy('catalog:product_list')
+
